c320062c9c4d8350b7fc9af0047eadad58da4b1c
lpavels <63040803+lpavels@users.noreply.github.com>
2022-01-27 10:06:28 +0700

    Delete backend/views directory

--- backend/views/users/menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-use yii\bootstrap4\Html;
-
-/*echo Html::a('<i class="bx bx-user"></i><span> Редактирование данных </span>', ['edit'],  ['class' => 'btn btn-primary btn-block']);*/
-echo Html::a('<i class="lni lni-reload"></i><span> Смена пароля</span>', ['change-password'], ['class' => 'btn btn-primary btn-block']);
